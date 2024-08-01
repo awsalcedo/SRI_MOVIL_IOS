ddd386146e307e8bc8db49e132fbe1d5523c81a1
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2024-07-31 20:24:02 -0500

    Initial commit

---
+++ SriMovil/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SriMovil
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 31/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("SRI, Móvil")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SriMovil/SriMovilApp.swift
@@ -0,0 +1,18 @@
+//
+//  SriMovilApp.swift
+//  SriMovil
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 31/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SriMovilApp: App {
+    var body: some Scene {
+        
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
