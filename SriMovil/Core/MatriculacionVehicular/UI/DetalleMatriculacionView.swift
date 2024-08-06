634ee04700161957fd0a08a6638ca1558dfdff42
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-08-06 16:31:37 -0500

    Se creó la pantalla de detalle del vehículo, junto con el botón del detalle en la pantalla principal, también se mejoró la vista de error tanto de pantalla exclusiva para los mensajes de error como usando un toast

---
+++ SriMovil/Core/MatriculacionVehicular/UI/DetalleMatriculacionView.swift
@@ -0,0 +1,18 @@
+//
+//  DetalleMatriculacionView.swift
+//  SriMovil
+//
+//  Created by usradmin on 6/8/24.
+//
+
+import SwiftUI
+
+struct DetalleMatriculacionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetalleMatriculacionView()
+}
---
+++ SriMovil/Core/MatriculacionVehicular/UI/MatriculacionVehicularToastView.swift
@@ -0,0 +1,18 @@
+//
+//  MatriculacionVehicularToastView.swift
+//  SriMovil
+//
+//  Created by usradmin on 6/8/24.
+//
+
+import SwiftUI
+
+struct MatriculacionVehicularToastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatriculacionVehicularToastView()
+}
