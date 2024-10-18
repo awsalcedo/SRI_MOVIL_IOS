e468b31eedda87ff16d232cb8380ad6c6f55c4fe
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-10-18 15:33:11 -0500

    Cambiar de color el fondo del mensaje de error, cambiar la view de Consultas al uso de cards

---
+++ SriMovil/Root/ConsultasCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ConsultasCardView.swift
+//  SriMovil
+//
+//  Created by usradmin on 17/10/24.
+//
+
+import SwiftUI
+
+struct ConsultasCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConsultasCardView()
+}
